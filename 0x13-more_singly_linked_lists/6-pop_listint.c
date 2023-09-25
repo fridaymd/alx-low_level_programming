0d716edb6ebaece55da31513d806e84d48c8f15d
fridaymd <fridaymd12@gmail.com>
2023-09-25 13:07:04 -0700

    pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,8 +1,10 @@
 #include "lists.h"
 
 /**
- * pop_listint
- * Reta inside the elements that was deleted,
+ * pop_listint - deletes the head node of a linked list
+ * @head: pointer to the first element in the linked list
+ *
+ * Return: the data inside the elements that was deleted,
  * or 0 if the list is empty
  */
 int pop_listint(listint_t **head)
